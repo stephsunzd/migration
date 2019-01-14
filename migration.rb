2d36340c73f311edda698ca132139b002fa1b9cc
stephsunzd <stsun@zendesk.com>
2019-01-14 14:55:14 -0800

    minor updates

--- migration.rb
+++ migration.rb
@@ -2,13 +2,10 @@ require 'erb'
 
 module Migration
   def self.generate_import_file(codes, source_url, items)
-    language_code = codes[:language]
-    country_code = codes[:country]
-
     posts_erb = File.open('templates/posts.xml.erb').read
     posts_erb = ERB.new(posts_erb)
 
-    out_file = File.new("import_files/posts_#{country_code}.xml", "w")
+    out_file = File.new("import_files/posts_#{codes[:country]}.xml", "w")
     out_file.puts(posts_erb.result(binding))
     out_file.close
   end
--- spec/migration_spec.rb
+++ spec/migration_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../migration'
 
-RSpec.describe "generate import file" do
+RSpec.describe "Migration" do
   TEST_CODES = { language: 'zd', country: 'zd' }
   TEST_URL = 'http://localhost:1337'
   TEST_ITEMS = [
@@ -10,12 +10,14 @@ RSpec.describe "generate import file" do
     }
   ]
 
-  it 'creates a file matching our sample' do
+  it '#generate_import_file creates a file matching our sample' do
     Migration.generate_import_file(TEST_CODES, TEST_URL, TEST_ITEMS)
 
-    posts_sample = File.open('spec/fixtures/posts_sample.xml').read
-    posts_zd = File.open('import_files/posts_zd.xml').read
+    posts_sample = File.open('spec/fixtures/posts_sample.xml').read.gsub(/\s/, '')
+    posts_zd = File.open('import_files/posts_zd.xml').read.gsub(/\s/, '')
 
     expect(posts_sample).to eq(posts_zd)
+
+    File.delete('import_files/posts_zd.xml')
   end
 end
