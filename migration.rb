b878dae9a298ea68a935355534ca5160785b26d4
stephsunzd <stsun@zendesk.com>
2019-01-14 16:28:05 -0800

    processing CSV data and getting closer

--- migration.rb
+++ migration.rb
@@ -1,7 +1,47 @@
+require 'csv'
 require 'erb'
 
 module Migration
-  def self.generate_import_file(codes, source_url, items)
+  def self.csv_to_item(language_code)
+    headers = []
+    items = []
+
+    CSV.foreach("metadata_files/#{language_code}.csv") do |row|
+      if headers.empty?
+        headers.replace(row)
+        next
+      end
+
+      item = {}
+
+      row.each_with_index do |value, index|
+        case headers[index]
+        when 'item_hidden'
+          item['post_status'] = value === 'TRUE' ? 'draft' : 'publish'
+        when 'item_tags'
+          item[headers[index]] = []
+
+          value.split(',').each do |tag_name|
+            item[headers[index]] << {
+              name: tag_name
+            }
+          end
+        else
+          item[headers[index]] = value
+        end
+
+#        if item['']
+      end
+
+      items << item
+    end
+
+    return items
+  end
+
+  def self.generate_import_file(codes, source_url)
+    items = self.csv_to_item(codes[:language])
+
     posts_erb = File.open('templates/posts.xml.erb').read
     posts_erb = ERB.new(posts_erb)
 
--- spec/migration_spec.rb
+++ spec/migration_spec.rb
@@ -2,19 +2,39 @@ require_relative '../migration'
 
 RSpec.describe "Migration" do
   TEST_CODES = { language: 'zd', country: 'zd' }
-  TEST_URL = 'http://localhost:1337'
-  TEST_ITEMS = [
-    {
-      'item_title' => 'Lorem ipsum',
-      'item_tags' => []
-    }
-  ]
+  TEST_URL = 'https://resources.zendesk.co.uk'
+  TEST_ITEM = {
+    'item_title' => 'Lorem ipsum',
+    'author_first_name' => nil,
+    'author_last_name' => nil,
+    'item_canonical_url' => 'https://www.zendesk.com/blog/10-customer-experience-kpis/',
+    'item_description' => 'Lorem ipsum dolor amet plaid slow-carb prism venmo kale chips. Lo-fi poke truffaut bushwick plaid. XOXO pug waistcoat edison bulb semiotics everyday carry succulents tbh hoodie literally mumblecore selvage. Intelligentsia tumblr gentrify, butcher venmo drinking vinegar readymade man bun ethical stumptown umami hoodie bespoke portland ennui. Aesthetic gastropub bitters pug unicorn pok pok.',
+    'item_hidden' => 'FALSE',
+    'item_id' => '888888888',
+    'item_published_at' => '2018-04-12 11:37:00',
+    'item_seo_description' => 'Lorem SEO ipsum description.',
+    'item_seo_title' => 'Lorem SEO ipsum',
+    'item_tags' => [
+      { name: 'Agent experience' },
+      { name: 'Best Practices' },
+      { name: 'Customer experience' }
+    ],
+    'item_thumbnail_url' => 'https://d26a57ydsghvgx.cloudfront.net/content/blog/customer_experience_KPIs.png',
+    'item_title' => 'Lorem ipsum',
+    'item_url' => 'https://resources.zendesk.co.uk/blog/10-customer-experience-kpis'
+  }
 
   it '#generate_import_file creates a file matching our sample' do
-    Migration.generate_import_file(TEST_CODES, TEST_URL, TEST_ITEMS)
+    item = Migration.csv_to_item(TEST_CODES[:language])
 
-    posts_sample = File.open('spec/fixtures/posts_sample.xml').read.gsub(/\s/, '')
-    posts_zd = File.open('import_files/posts_zd.xml').read.gsub(/\s/, '')
+    expect(item).to eq([TEST_ITEM])
+  end
+
+  it '#generate_import_file creates a file matching our sample' do
+    Migration.generate_import_file(TEST_CODES, TEST_URL)
+
+    posts_sample = File.open('spec/fixtures/posts_sample.xml').read.gsub(' ', '')
+    posts_zd = File.open('import_files/posts_zd.xml').read.gsub(' ', '')
 
     expect(posts_sample).to eq(posts_zd)
 
