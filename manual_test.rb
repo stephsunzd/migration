c5e8b0902d61b40e81b365998920c982df340405
stephsunzd <stsun@zendesk.com>
2019-08-02 11:21:03 -0700

    update resource image selector

--- manual_test.rb
+++ manual_test.rb
@@ -3,9 +3,10 @@ require_relative 'scraper'
 item = {
   Constants::KEYS[:id] => '201900001',
   'item_published_at' => '2019-02-04 12:00:00',
-  Constants::KEYS[:url] => 'https://www.zendesk.com.mx/support/webinar/whats-new-product-updates-dec-2018-amer',
+  Constants::KEYS[:url] => 'https://www.zendesk.es/resources/5-biggest-gaps-customer-service-midsize-companies',
   'post_status' => 'publish',
-  Constants::KEYS[:type] => 'webinar',
+  Constants::KEYS[:type] => 'resource',
+  'item_tags' => []
 }
 
 item = Scraper.scrape_post(item)
